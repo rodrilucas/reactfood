688e7472a0f276f02fe34a4a95e733bafe1d471d
Lucas Rodrigues Silva <lucas.celery@gmail.com>
2022-11-14 08:12:19 -0300

    add modal via react portal, managing Cart e ModalState

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 
 import Header from "./Layout/Header";
 import Meals from "./Meals/Meals";
+import Cart from "./Cart/Cart";
 
 function App() {
+
+  const [cartIsShow, setCartIsShow] = useState(false);
+
+  const showCartHanlder = () => {
+    setCartIsShow(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShow(false);
+  };
+
   return (
     <Fragment>
-      <Header />
+      {cartIsShow && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCartHanlder}/>
       <main>
         <Meals />
       </main>
